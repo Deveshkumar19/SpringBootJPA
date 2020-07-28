e5a0a3778b5e7b8d380b31e58ef216a6e348e01b
Devesh Kumar <deveshkr271988@gmail.com>
2020-07-28 18:20:50 +0200

    swagger enabled

--- src/main/java/com/Devesh/BookAPI/BookApiApplication.java
+++ src/main/java/com/Devesh/BookAPI/BookApiApplication.java
@@ -2,8 +2,10 @@ package com.Devesh.BookAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookApiApplication {
 
 	public static void main(String[] args) {
